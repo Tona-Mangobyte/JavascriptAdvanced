3525d0103a77f8b691e21abbf04e5563b43ee375
Tona <tona@mango-byte.com>
2024-05-02 16:56:08 +0700

    fix

--- ch4-functional-programming/partial-application-and-currying/url-builder.js
+++ ch4-functional-programming/partial-application-and-currying/url-builder.js
@@ -1,14 +1,18 @@
 const apiUrl = 'https://www.googleapis.com/books/volumes?q=12345';
 
-function url(protocol, subdomain, domain, pathSegment) {
+function uri(protocol, subdomain, domain, pathSegment) {
   return `${protocol}://${subdomain}.${domain}/${pathSegment.join('/')}`;
+}
+
+function url(protocol, subdomain, domain, pathSegment, querySegment) {
+  return `${protocol}://${subdomain}.${domain}/${pathSegment.join('/')}?${querySegment.join('&')}`;
 }
 
 // Step 1
 // const partialUrl = url.bind(null, 'https', 'www', 'googleapis.com');
-// const fullUrl = partialUrl(['books', 'volumes', 'q=12345']);
-// const fullUrl = url.call(null, 'https', 'www', 'googleapis.com', ['books', 'volumes', 'q=12345']);
-const fullUrl = url.apply(null, ['https', 'www', 'googleapis.com', ['books', 'volumes', 'q=12345']]);
+// const fullUrl = partialUrl(['books', 'volumes'], ['q=12345']);
+// const fullUrl = url.call(null, 'https', 'www', 'googleapis.com', ['books', 'volumes'],['q=12345']);
+const fullUrl = url.apply(null, ['https', 'www', 'googleapis.com', ['books', 'volumes'], ['q=12345']]);
 console.log(fullUrl); // Outputs: https://www.googleapis.com/books/volumes/q=12345
 
 // Step 2
@@ -17,6 +21,6 @@ function partial(func, ...args) {
     return func(...args, ...newArgs);
   }
 }
-const partialUrl = partial(url, 'https', 'www', 'googleapis.com');
-const fullUrl2 = partialUrl(['books', 'volumes', 'q=12345']);
-console.log(fullUrl2); // Outputs: https://www.googleapis.com/books/volumes/q=12345
+const partialUrl = partial(uri, 'https', 'www', 'googleapis.com');
+const fullUri = partialUrl(['books', 'volumes']);
+console.log(fullUri); // Outputs: https://www.googleapis.com/books/volumes
